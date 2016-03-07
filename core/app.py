db262dec55ae097c376f58154304e4605cd76e53
lijiaping <lijiaping@gene-health.com>
2016-03-07 17:08:51 +0800

    load and new

--- core/app.py
+++ core/app.py
@@ -1,19 +1,25 @@
 import yaml
+import os
 
 class App(object):
     """Everything about App"""
     def __init__(self, app_path):
         super(App, self).__init__()
         self.app_path = app_path
+        self.config_file = app_path + '/config.yaml'
         self.parameters = {}
         self.config = {}
 
-    def load(self, app_path):
-        config_file = file(app_path, 'r')
-        self.config = yaml.load(config_file)
+    def load(self):
+        with open(self.config_file, 'r') as config_file:
+            self.config = yaml.load(config_file)
 
     def new(self):
-        pass
+        for folder in ['bin', 'lib', 'test']:
+            os.makedirs("%s/%s" % (self.app_path, folder))
+        open(self.app_path + '/Dockerfile', 'a').close()
+        with open(self.config_file, 'w') as config_file:
+            yaml.dump(self.config, config_file)
 
     def build(self):
         pass
--- test/test_app.py
+++ test/test_app.py
@@ -3,11 +3,19 @@ from core.app import App
 
 class TestApp(unittest.TestCase):
     """docstring for TestApp"""
-    def SetUp(self):
-        self.app = App('test')
+    # def setUp(self):
+    #     pass
+        #self.app = App('load_app')
 
     def test_load(self):
-        self.app.load('test/test_app.yaml')
+        app = App('test/load_app')
+        app.load()
+        print app.config
+
+    def test_new(self):
+        app = App('test/new_app')
+        app.new()
+        print app.config
 
 if __name__ == '__main__':
     unittest.main()
