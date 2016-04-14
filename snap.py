05f67a7e0f248b6677bedb570045afffeaf5dd16
lijiaping <lijiaping@gene-health.com>
2016-04-14 09:27:20 +0800

    app add node command

--- snap.py
+++ snap.py
@@ -14,7 +14,7 @@ def new_app(args):
         subparsers_app.print_help()
         print >> sys.stderr, "app name is not optional."
 
-def build_app(args):
+def init_app(args):
     if(args.name):
         app = App(args.name)
     elif(args.config):
@@ -25,8 +25,19 @@ def build_app(args):
         subparsers_app_build.print_help()
         print >> sys.stderr, "app name or config.yaml must be specified."
         os._exit(0)
+    return app
 
+def build_app(args):
+    app = init_app(args)
     app.build(args.param, args.out)
+
+def node_app(args):
+    app = init_app(args)
+    if args.type == 'all':
+        app.buildTestWorkflow(sys.stdout)
+    else:
+        nodes = app.nodes(args.type)
+        app.dumpYaml(nodes, args.out)
 
 if __name__ == "__main__":
     parsers = argparse.ArgumentParser(
@@ -61,9 +72,25 @@ if __name__ == "__main__":
         formatter_class=argparse.RawTextHelpFormatter)
     subparsers_app_build.add_argument('-name', help = "app name")
     subparsers_app_build.add_argument('-config', help = "config.yaml file")
-    subparsers_app_build.add_argument('-param', help = "render from parameter.yaml file")
+    subparsers_app_build.add_argument('-param', help = "render from parameter.yaml file. default will be use if not specified.")
     subparsers_app_build.add_argument('-out', help = "output render result to file. default write to stdout")
     subparsers_app_build.set_defaults(func=build_app)
+    #app node
+    subparsers_app_node = subparsers_app.add_parser('node',
+        help='APP workflow node template',
+        description="This command can render config.yaml into workflow nodes."
+        "Without `-out`, result will be writed to STDOUT",
+        prog='snap app node',
+        formatter_class=argparse.RawTextHelpFormatter)
+    subparsers_app_node.add_argument('-name', help = "app name")
+    subparsers_app_node.add_argument('-config', help = "config.yaml file")
+    subparsers_app_node.add_argument('-type', choices=['all', 'app', 'load', 'store'],
+        help = "`all`: test workflow; "
+        "`app`: app node only; "
+        "`load`: load node only; "
+        "`store`: store node only; ")
+    subparsers_app_node.add_argument('-out', help = "output render result to file. default write to stdout")
+    subparsers_app_node.set_defaults(func=node_app)
 
 if __name__ == '__main__':
     argslist = sys.argv[1:]
