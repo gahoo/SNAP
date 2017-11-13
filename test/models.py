1dd922a7caf1fc4044a57273c51a3fb51101e39d
lijiaping <lijiaping@igenecode.com>
2017-11-13 11:06:24 +0800

    tests for db

---
+++ test/models.py
@@ -0,0 +1,18 @@
+from core.models import Project
+import unittest
+import shutil
+import pdb
+
+class TestProject(unittest.TestCase):
+    """docstring for TestProject"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_path(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
