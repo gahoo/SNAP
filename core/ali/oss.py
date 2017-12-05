6a80b08e2c05b886c7ccecaffe1d5801773766b2
lijiaping <lijiaping@igenecode.com>
2017-12-05 10:26:55 +0800

    add oss2key

--- core/ali/oss.py
+++ core/ali/oss.py
@@ -1,5 +1,11 @@
 import oss2
+import os
 from . import ALI_CONF
+
+def oss2key(destination):
+    prefix = os.path.join('oss://', BUCKET.bucket_name)
+    key = destination.replace(prefix, '').strip('/')
+    return key
 
 endpoint = "http://oss-%s.aliyuncs.com" % ALI_CONF['region']
 AUTH = oss2.Auth(ALI_CONF['accesskey_id'], ALI_CONF['accesskey_secret'])
