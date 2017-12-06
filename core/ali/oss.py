7e03c1d83f8bac24e40b4583a21933a316946432
lijiaping <lijiaping@igenecode.com>
2017-12-06 09:56:59 +0800

    add OSSkeys iterator to deal with keys more than 1000

--- core/ali/oss.py
+++ core/ali/oss.py
@@ -7,6 +7,29 @@ def oss2key(destination):
     key = destination.replace(prefix, '').strip('/')
     return key
 
+class OSSkeys(object):
+    def __init__(self, array, step = 1000):
+        self.idx = 0
+        self.array = array
+        self.step = step
+        self.length = len(array)
+        self.n = self.length / self.step
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.idx < self.n:
+            val = self.array[self.idx * self.step:self.step * (self.idx + 1)]
+            self.idx = self.idx + 1
+            return val
+        elif self.idx == self.n:
+            val = self.array[self.idx * self.step:]
+            self.idx = self.idx + 1
+            return val
+        else:
+            raise StopIteration()
+
 endpoint = "http://oss-%s.aliyuncs.com" % ALI_CONF['region']
 AUTH = oss2.Auth(ALI_CONF['accesskey_id'], ALI_CONF['accesskey_secret'])
 BUCKET = oss2.Bucket(AUTH, endpoint, ALI_CONF['bucket'])
