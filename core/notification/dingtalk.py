5866fe2b4bab70b76daec31afa6d1fbdd6677e4e
lijiaping <lijiaping@igenecode.com>
2018-03-19 17:30:57 +0800

    add dingtalk related notification

---
+++ core/notification/dingtalk.py
@@ -0,0 +1,27 @@
+from . import DINGTALK_CONF
+import requests
+import json
+
+def send_msg(content, msgtype='markdown', contact=None, **kwargs):
+    if not DINGTALK_CONF:
+        return
+    if not contact:
+        contact=DINGTALK_CONF['mobile']
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=' + DINGTALK_CONF['access_token']
+    headers = {'Content-type': 'application/json'}
+    data = {
+    'msgtype': msgtype,
+    msgtype: build_msg_content(content=content, msgtype=msgtype, **kwargs),
+    'at': {'atMobiles': [contact]}
+    }
+    resp = requests.post(url, headers=headers, data=json.dumps(data))
+    return resp
+
+def build_msg_content(content, msgtype, title='', msg_url='', pic_url='', **kwargs):
+    if msgtype == 'markdown':
+        data = {'title': title, 'text': content}
+    elif msgtype == 'text':
+        data = {'content': content}
+    elif msgtype == 'link':
+        data = {'title': title, 'text': content, 'messageUrl': msg_url, 'picUrl': pic_url}
+    return data
